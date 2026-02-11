b23ce723ac67199d2582bbb7d93697b37c57b057
Pov Davin <davin@Povs-MacBook-Pro.local>
2026-02-11 11:00:46 +0700

    BLA-001 correct code

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -2,36 +2,39 @@ import '../data/dummy_data.dart';
 import '../model/ride/locations.dart';
 import '../model/ride/ride.dart';
 
-////
-///   This service handles:
-///   - The list of available rides
-///
 class RidesService {
   static List<Ride> availableRides = fakeRides;
-
-  static List<Ride> filterByDeparture(Location departure) {
-    List<Ride> rides = availableRides
-        .where((e) => e.departureLocation == departure)
-        .toList();
-    return rides;
+  //
+  //  filter the rides starting from given departure location
+  //
+  static List<Ride> _filterByDeparture(List<Ride> rides, Location departure) {
+    return rides.where((ride) => ride.departureLocation == departure).toList();
   }
 
-  static List<Ride> filterBySeatRequested(int seatRequested) {
-    List<Ride> rides = availableRides
-        .where((e) => e.availableSeats >= seatRequested)
-        .toList();
-    return rides;
+  //
+  //  filter the rides starting for the given requested seat number
+  //
+  static List<Ride> _filterBySeatRequested(
+    List<Ride> rides,
+    int requestedSeat,
+  ) {
+    return rides.where((ride) => ride.availableSeats >= requestedSeat).toList();
   }
 
+  //
+  //  filter the rides   with several optional criteria (flexible filter options)
+  //
   static List<Ride> filterBy({Location? departure, int? seatRequested}) {
-    List<Ride> rides = [];
-    if (departure != null && seatRequested == null) {
-      rides = availableRides.where((e) => e.departureLocation == departure).toList();
-    } else if (departure == null && seatRequested != null) {
-      rides = availableRides.where((e) => e.availableSeats >= seatRequested).toList();
-    } else {
-      rides = availableRides;
+    List<Ride> result = availableRides;
+
+    if (departure != null) {
+      result = _filterByDeparture(result, departure);
     }
-    return rides;
+
+    if (seatRequested != null) {
+      result = _filterBySeatRequested(result, seatRequested);
+    }
+
+    return result;
   }
 }
